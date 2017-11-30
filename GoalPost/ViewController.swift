e27834a312136bfda0f0ee2704b917eb7f7a609d
Mohamed SADAT <moh12594@gmail.com>
2017-11-30 09:26:41 +0100

    initial commit

---
+++ GoalPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalPost
+//
+//  Created by Mohamed SADAT on 30/11/2017.
+//  Copyright © 2017 Mohamed SADAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
